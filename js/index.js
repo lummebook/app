4c6bfa8a2c4dfc4f0d22930c6c4d02f31beb468d
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-10-26 11:25:28 -0300

    Visuais da tela de Landing Page e de Login terminados, faltando integração com back-end

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Cria a função para trocar para página de login
+function abrirLogin() {
+    window.location.href = "pages/login.html";
+}
+
+// Cria a função para trocar para página de cadastro
+function abrirCadastro() {
+    window.location.href = "pages/cadastro.html";
+}
