32d5eb705a96b704f4378a95b20bc8859ce0d98c
GabAlmeyda <gabrielalmeida7258@gmail.com>
2025-11-15 16:07:49 -0300

    Integração com o backend para retornar todos os livros registrados

--- js/home.js
+++ js/home.js
@@ -1,5 +1,79 @@
+// Cria a função para chamar outras funções assim que a página carregar
+function executarFuncoes() {
+    // Chama a função para carregar informações do usuário
+    carregarConfiguracoes();
+
+    // Chama a função para retornar os livros registrados
+    carregarLivrosRegistrados();
+}
+
+// Cria a função para retornar os livros registrados
+async function carregarLivrosRegistrados() {
+    // Pega os elementos do container e da mensagem de erro
+    const erroContainer = document.getElementById("livros__erro");
+    const erroMensagem = document.getElementById("erro__mensagem");
+
+    // Mostra a mensagem de carregamento na tela
+    erroMensagem.style.color = "var(--cor-texto-secundaria)";
+    erroMensagem.textContent = "Carregando...";
+
+    // Tenta retornar os livros registrados
+    try {
+        const resposta = await fetch("http://localhost:8080/livros");
+
+        // Transforma os dados retornados
+        const livrosRetornados = await resposta.json();
+
+        // Esconde a mensagem de erro
+        erroContainer.style.display = "none";
+
+        // Pega o container dos livros
+        const livrosContainer = document.getElementById("livros__container");
+
+        // Percorre todos os livros retornados
+        for (const livro of livrosRetornados) {
+            // Cria o container do livro
+            const div = document.createElement("div");
+
+            // Adiciona a classe para estilizar
+            div.classList.add("livro");
+
+            // Adiciona o ID do livro
+            div.dataset.id = livro.idLivro;
+
+            // Adiciona os elementos internos
+            div.innerHTML = `
+                <div class="livro__imagem"></div>
+                <h3 class="livro__titulo">${livro.titulo}</h3>
+                <h4 class="livro__autor">${livro.autor}</h4>
+                <p class="livro__preco">R$${livro.preco.toFixed(2)}</p>
+                <p class="livro__quant">${livro.quantidade} em estoque</p>
+                <div class="livro__botao">
+                    <button class="global__botao-primario livro__botao-detalhes">Detalhes</button>
+                </div>
+                <div class="livro__botao">
+                    <button class="global__botao-secundario livro__botao-carrinho">Para o carrinho</button>
+                </div>
+            `;
+
+            // Adiciona a função do botão
+            div.querySelector(".livro__botao").addEventListener("click", () =>
+                abrirDetalhes(livro.id)
+            );
+
+            // Adiciona o container do livro no HTML
+            livrosContainer.appendChild(div);
+        }
+    } catch (erro) {
+        erroMensagem.style.color = "#ca0101";
+        erroMensagem.textContent =
+            "Houve um erro ao tentar carregar os Livros. Tente novamente.";
+        console.log(erro);
+    }
+}
+
 // Cria a função para carregar as informações do usuário
-async function carregarConfiguracoes () {
+async function carregarConfiguracoes() {
     // Pega o ID do usuário salvo se existir
     const idUsuario = JSON.parse(localStorage.getItem("idUsuario"));
 
@@ -29,7 +103,7 @@ async function carregarConfiguracoes () {
 }
 
 // Cria a função para desconectar o usuário
-function desconectarUsuario (event) {
+function desconectarUsuario() {
     // Limpa todos os dados registrados
     localStorage.clear();
 
@@ -38,7 +112,7 @@ function desconectarUsuario (event) {
 }
 
 // Cria a função para trocar para a página de vender um livro
-function venderLivro () {
+function venderLivro() {
     // Troca para a tela de vender livro
-    window.location.href = "livro.html";
+    window.location.href = "venda.html";
 }
--- js/venda.js
+++ js/venda.js
@@ -7,31 +7,35 @@ function abrirHome() {
 async function venderLivro(event) {
     event.preventDefault();
 
+    // Pega o parágrafo da mensagem de erro
+    const vendaMensagem = document.getElementById("erro__mensagem");
+
+    vendaMensagem.textContent = ""; // Tira a mensagem
+    vendaMensagem.style.opacity = "0"; // Esconde a mensagem da tela
+
     // Pega o valor dos campos do formulário
     const titulo = document.getElementById("form__input-titulo").value.trim();
     const autor = document.getElementById("form__input-autor").value.trim();
     const preco = document.getElementById("form__input-preco").value;
-    const quantidade = document.getElementById("form__input-quant").value;
-
-    const vendaMensagem = document.getElementById("erro__mensagem");
+    const quantidade = Number(document.getElementById("form__input-quant").value);
 
     // Bloco try/catch para impedir erro de aparecer para o usuário
     try {
+        const idVendedor = localStorage.getItem("idUsuario");
+
         // Realiza a requisição para colocar o livro à venda
-        await fetch("http://localhost:8080/livros/", {
+        const resposta = await fetch("http://localhost:8080/livros", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                data: { titulo, autor, preco, quantidade },
+                data: { idVendedor, titulo, autor, preco, quantidade },
             }),
         });
 
         abrirHome(); // Volta para página inicial
-    }
-    // Caso a requisição der erro, cai no catch
-    catch (erro) {
+    } catch (erro) {
         // Muda a mensagem
         vendaMensagem.textContent = "Erro na requisição. Tente novamente.";
 
